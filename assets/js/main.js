765d2ae2417bd514ff99fe7915235c34d52fba4d
ahmadsoni <ahmadshonhaji476@gmail.com>
2021-12-05 08:22:09 +0700

    semi final fiture

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,20 +8,6 @@ $(document).ready(function() {
             }  
         });
             
-    /* ----- Video Popup------- */
-    const videoSrc = $('#player-1').attr("src");
-    $(".vidio-play-btn, .video-popup").on("click", function() {
-        if($(".video-popup").hasClass("open")){
-            $(".video-popup").removeClass("open");
-            $("#player-1").attr("src", "");
-        }
-        else{
-            $(".video-popup").addClass("open");
-            if($("#player-1").attr("src") == ""){
-                $("#player-1").attr("src", videoSrc);
-            }
-        }
-    });
     /* ----------Features carousel------ */
     $('.features-carousel').owlCarousel({
         loop:true,
